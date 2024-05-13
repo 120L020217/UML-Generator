4bb89ecb6fa21c4334610d1a99af621d2bfba1bd
coldcodacode <gzs_0217@qq.com>
2024-05-13 18:33:00 +0800

    集成文本分析模块

--- src/components/views/DemandExtraction.vue
+++ src/components/views/DemandExtraction.vue
@@ -1,3 +1,25 @@
 <template>
-    
+    <div class="app-containers">
+        <iframe :src="reactAppUrl" style="width: 100%; height: 100%; border: none;"></iframe>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            reactAppUrl: 'http://localhost:3000'
+        };
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-containers {
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 0px);
+    // height: 100%;
+    overflow: hidden;
+}
+</style>
